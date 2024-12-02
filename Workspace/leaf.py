81059b58b7b1580a379c3e2f32d4c38b2adbeb05
Koromonnnnnnnn <764363@dpsk12.net>
2024-12-02 10:32:23 -0700

    .

--- Workspace/leaf.py
+++ Workspace/leaf.py
@@ -1,14 +1,22 @@
 import random
-import pygame 
+import pygame
 
 img = pygame.image.load("leaf.png")
+
 
 class leaf:
     def __init__(self, xpos, ypos):
         self.xpos = xpos
         self.ypos = ypos
-        self.color = (random.randrange(100,250), random.randrange(100,250), random.randrange(100,250))
-    def draw(self, screen):
-        img = self.color(random.randrange(100,250), random.randrange(100,250), random.randrange(100,250))
-        screen.blit(img, (random.randrange(-100, 800),random.randrange(-100, 800)))
+        self.color = (
+            random.randrange(100, 250),
+            random.randrange(100, 250),
+            random.randrange(100, 250),
+        )
 
+    def draw(self, screen, xpos, ypos, amount):
+        xpos = (random.randrange(100, 250), random.randrange(100, 250))
+        ypos = (random.randrange(100, 250), random.randrange(100, 250))
+
+        for i in range(amount):
+            screen.blit(img, xpos, ypos)
