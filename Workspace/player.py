f04292fe58346fa1d5ee389ac628eb12a6fef2aa
Koromonnnnnnnn <764363@dpsk12.net>
2024-12-13 08:44:21 -0700

    .

--- Workspace/player.py
+++ Workspace/player.py
@@ -1,14 +1,20 @@
 # player.py
 import pygame
 
+leafblower = pygame.image.load("leafblower.png")
+leafblower = pygame.transform.scale(leafblower, (150, 150))
+
+
 class player:
     def __init__(self, image, xpos=0, ypos=0):
         self.xpos = xpos
         self.ypos = ypos
         self.image = image
+        self.leafblower = leafblower
 
     def drawPlayer(self, screen):
         screen.blit(self.image, (self.xpos, self.ypos))
+        screen.blit(self.leafblower, (self.xpos - 90, self.ypos))
 
     def updatePostion(self, x, y):
         self.xpos = x
